5a1653f361d8a2a764eeb0bc5ba2a26a29ca7091
Antony Blakey <antony.blakey@gmail.com>
2025-02-15 15:05:23 +1100

    Fix overlay expose

--- flake.nix
+++ flake.nix
@@ -1,26 +1,10 @@
-# flake.nix
 {
-  description = "Custom package collection";
-
-  inputs = {
-    nixpkgs.url = "github:NixOS/nixpkgs/nixos-unstable";
-    flake-utils.url = "github:numtide/flake-utils";
-  };
-
-  outputs = { self, nixpkgs, flake-utils }:
+  outputs =
+    _:
     let
       overlay = import ./overlays;
     in
     {
       overlays.default = overlay;
-    } // flake-utils.lib.eachDefaultSystem (system: {
-      packages = {
-        racket-minimal =
-          (import nixpkgs {
-            inherit system;
-            overlays = [ overlay ];
-          }).racket-minimal;
-        default = self.packages.${system}.racket-minimal;
-      };
-    });
+    };
 }
